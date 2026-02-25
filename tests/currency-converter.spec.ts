6c23c1f044a36ad3401e78b867fbc33f970c5522
EllaPellaa <112462853+EllaPellaa@users.noreply.github.com>
2026-02-25 22:50:11 +0200

    Add negative test for successful conversion

--- tests/currency-converter.spec.ts
+++ tests/currency-converter.spec.ts
@@ -1,5 +1,16 @@
 import { test, expect } from '@playwright/test';
 
+// Add alerts to an array and don't show them normally
+async function stubAlerts(page: import('@playwright/test').Page) {
+  await page.addInitScript(() => {
+    (window as any).__alerts = [];
+    const originalAlert = window.alert;
+    window.alert = (msg?: any) => {
+      (window as any).__alerts.push(String(msg ?? ''))
+    };
+  });
+}
+
 test.describe('Successful conversion', () => {
   test('Should convert EUR to SEK successfully', async({ page }) => {
     await page.goto('/')
@@ -12,5 +23,16 @@ test.describe('Successful conversion', () => {
 
     const output = page.getByLabel('SEK')
     await expect(output).toContainText(/^\d+(\.\d+)?$/)
+  })
+})
+
+test.describe('Error handling', () => {
+  test('Should display error with empty input', async({ page }) => {
+    await stubAlerts(page)
+    await page.goto('/')
+    await page.getByRole('button', { name: 'Convert' }).click()
+    await page.waitForFunction(() => (window as any).__alerts as string[]);
+    const alerts = await page.evaluate(() => (window as any).__alerts as string[]);
+    expect(alerts[0]).toContain('Please enter a valid number');
   })
 })
