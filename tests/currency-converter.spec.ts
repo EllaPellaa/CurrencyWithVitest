6229343664c834e5c8d39d67b4219b49880bfe34
EllaPellaa <112462853+EllaPellaa@users.noreply.github.com>
2026-02-25 22:39:44 +0200

    Successful conversion test with playwright

--- tests/currency-converter.spec.ts
+++ tests/currency-converter.spec.ts
@@ -1,2 +1,16 @@
 import { test, expect } from '@playwright/test';
 
+test.describe('Successful conversion', () => {
+  test('Should convert EUR to SEK successfully', async({ page }) => {
+    await page.goto('/')
+
+    const responsePromise = page.waitForResponse('**/api/convert/amount')
+    await page.getByLabel('EUR').fill('100')
+    await page.getByRole('button', { name: 'Convert' }).click()
+
+    await responsePromise
+
+    const output = page.getByLabel('SEK')
+    await expect(output).toContainText(/^\d+(\.\d+)?$/)
+  })
+})
